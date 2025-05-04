323cbd0ff4c5bfd5189e4bbeaa6bb13658587137
github_username_here <email@example.com>
2025-05-04 10:25:47 +0200

    Added sort_list function to stats.py, refactored count_charachters function

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
 def main(path_to_file):
     file_contents = get_book_text(path_to_file)
-    word_count = import_words(file_contents)
-    print(f"{word_count} words found in the document")
-    print(count_charachters(file_contents))
+    sorted_list = sort_list(count_charachters(file_contents))
+    print(f"{import_words(file_contents)} words found in the document")
+    for i in sorted_list:
+        if i['char'].isalpha():
+            print(f"{i['char']}: {i['num']}")
 
 def get_book_text(file):
     with open(file) as f:
         file_contents = f.read()
     return file_contents
     
-def count_charachters(file_as_string):
-    lowercase_string = file_as_string.lower()
-    charachter_list = {}
-    for charachter in lowercase_string:
-        if charachter in charachter_list:
-            charachter_list[charachter] += 1
-        else:
-            charachter_list[charachter] = 1
-    return charachter_list
-
-from stats import import_words
+from stats import import_words, count_charachters, sort_list
     
 main("books/frankenstein.txt")
--- stats.py
+++ stats.py
@@ -1,3 +1,23 @@
 def import_words(text):
     words = len(text.split())
     return words
+
+def count_charachters(file_as_string):
+    lowercase_string = file_as_string.lower()
+    charachter_list = {}
+    for charachter in lowercase_string:
+        if charachter in charachter_list:
+            charachter_list[charachter] += 1
+        else:
+            charachter_list[charachter] = 1
+    return charachter_list
+
+def sort_list(charachter_list):
+    def sort_on(dict):
+        return dict["num"]
+    letters = []
+    for entry in charachter_list:
+        dict = {"char": entry, "num": charachter_list[entry]}
+        letters.append(dict)
+    letters.sort(reverse=True, key=sort_on)
+    return letters
